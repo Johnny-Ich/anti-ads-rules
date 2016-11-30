7c51ecde35ca52203001e183639a6e272075e6da
Johnny-Ich <jonathan-schubert@hotmail.de>
2016-11-30 16:58:59 +0100

    Update anti-ads-rules.sh
    Changed Action from DROP to REJECT
    Added correct Progresspercentage

--- anti-ads-rules.sh
+++ anti-ads-rules.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Autor: 	Carl Jonathan Schubert
+# GitHub:	https://github.com/Johnny-Ich/anti-ads-rules/
 # E-Mail: 	jonathan-schubert@gcraft.eu
 # E-Mail #2: 	jonathan@eseltritt.de
+# License: 	GNU GENERAL PUBLIC LICENSE Version 3
 
 typeset -i i
 typeset -i linestart
@@ -10,6 +12,7 @@ typeset -i lineend
 typeset -i linedelete
 typeset -i linedeleteend
 typeset -i lineinsert
+typeset -i linestmpfile
 
 iptables-save > /etc/iptables/rules.v4
 cp /etc/iptables/rules.v4 /etc/iptables/rules.v4_BACKUP
@@ -40,13 +43,13 @@ else
 fi
 
 wget -q -O tmpfile "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
-touch tmpfile
+linestmpfile=&(cat tmpfile | wc -l)
 cat tmpfile | while read line; do
 	host $line | grep 'has address' | cut -d ' ' -f 4 | while read adblockIP; do
-		sed -i "$(echo "$linestart")a$(echo "-A FORWARD -d $adblockIP -o eth0 -j DROP")" rules.v4
+		sed -i "$(echo "$linestart")a$(echo "-A FORWARD -d $adblockIP -o eth0 -j REJECT --reject-with icmp-host-prohibited")" rules.v4
 	done
 	i=$i+1
-	progress=$(echo "scale=2;$i/3000*100" | bc | cut -d . -f 1)
+	progress=$(echo "scale=2;$i/$linestmpfile*100" | bc | cut -d . -f 1)
 	echo -ne "\rProgress: $progress%"
 done
 cp rules.v4 /etc/iptables/rules.v4
