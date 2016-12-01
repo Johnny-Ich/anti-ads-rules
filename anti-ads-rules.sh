d5cabc2ecd5988cd2dd414295d8c9f5ab533d780
Johnny-Ich <jonathan-schubert@hotmail.de>
2016-12-01 11:39:38 +0100

    Some formatting

--- anti-ads-rules.sh
+++ anti-ads-rules.sh
@@ -44,14 +44,18 @@ fi
 
 wget -q -O tmpfile "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
 linestmpfile=$(cat tmpfile | wc -l)
-cat tmpfile | while read line; do
-	host $line | grep 'has address' | cut -d ' ' -f 4 | while read adblockIP; do
+
+cat tmpfile | while read line
+do
+	host $line | grep 'has address' | cut -d ' ' -f 4 | while read adblockIP
+	do
 		sed -i "$(echo "$linestart")a$(echo "-A FORWARD -d $adblockIP -o eth0 -j REJECT --reject-with icmp-host-prohibited")" rules.v4
 	done
 	i=$i+1
 	progress=$(echo "scale=2;$i/$linestmpfile*100" | bc | cut -d . -f 1)
 	echo -ne "\rProgress: $progress%"
 done
+
 cp rules.v4 /etc/iptables/rules.v4
 iptables-restore < /etc/iptables/rules.v4
 rm tmpfile
